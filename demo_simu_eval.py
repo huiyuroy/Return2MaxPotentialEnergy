7c4cdef289f24408caf040ccbbc7c23576d1802f
lhyrr <huiyuroy@163.com>
2025-06-15 16:50:58 +0800

    updata

--- demo_simu_eval.py
+++ demo_simu_eval.py
@@ -12,47 +12,45 @@ from pyrdw.core.env.base import RdwEnv
 if __name__ == '__main__':
     cur_path = os.path.abspath(os.path.dirname(__file__))
     root_path = cur_path[:cur_path.find('Return2MaxPotentialEnergy') + len('Return2MaxPotentialEnergy')]
-    # 1.创建环境
+    # 1.create env
     env = RdwEnv()
-    # 2.添加agents，必须在env创建后调用，例子中加载了no+r21和no+r2mpe算法
-    env.add_agent(default.obtain_agent(gainer='simple', rdwer='s2c', resetter='r2mpe', inputer='traj'),
-                  name='r2mpe')
-
+    # 2.add agents
+    env.add_agent(default.obtain_agent(gainer='simple', rdwer='s2c', resetter='r2mpe', inputer='traj'), name='r2mpe')
     env.load_logger()  # 加载日志记录器，可以自己设计
     env.load_ui()  # 加载ui界面，可以自己设计
 
-    # 3.加载物理空间
+    # 3.load phy space
     p_path = data_path + '\\phy'
     pname = 'test0'
     pscene = generator.load_scene(p_path + '\\' + pname + '.json')
     pscene.update_grids_runtime_attr()
-    pscene.calc_r2mpe_precomputation()  # 如果采用R2mpe的任何组件，该函数必须调用以激活场景位置点可见区域离线计算
+    pscene.calc_r2mpe_precomputation()  # if use r2mpe, must call this precomputation method
 
+    # 4. load vir space, can load one or a batch
     v_path = data_path + '\\vir'
     v_files = [os.path.join(v_path, file) for file in os.listdir(v_path) if 'json' in file]
 
     for v_scene_path in v_files:
-        # 4.加载虚拟空间,这里批量化加载，也可加载一个
         _, v_scene_name = os.path.split(v_scene_path)
         v_name = v_scene_name.split(".")[0]
         vscene = generator.load_scene(v_path + '\\' + v_name + '.json')
         vscene.update_grids_runtime_attr()
-        vscene.calc_r2mpe_precomputation()  # 如果采用R2mpe的任何组件，该函数必须调用以激活场景位置点可见区域离线计算
+        vscene.calc_r2mpe_precomputation()  # if use r2mpe, must call this precomputation method
         print(v_name)
 
-        # 5.设置环境虚拟和真实空间
-        env.set_scenes(vscene, pscene)  # 设置虚拟和物理空间，必须在prepare前调用
+        # 5.setup vir and phy spaces for env
+        env.set_scenes(vscene, pscene)
 
-        # 6.加载虚拟空间模拟行走路径
+        # 6.load vir trajectories
         vtrajs = generator.load_trajectories(v_path, v_name)
         for vtraj in vtrajs:
-            vtraj.range_targets(0, 500)  # 最长走编号0-500之间的目标，可自行调整
-        env.set_trajectories(vtrajs)  # 设置模拟路径，仅在模拟重定向模式下调用
+            vtraj.range_targets(0, 500)
+        env.set_trajectories(vtrajs)  # setup trajectory for an env
 
-        # 7.env组件预处理，如果更换场景或者更换模拟路径后必须调用
-        env.prepare()  # 加载预处理内容，例如场景可见性划分、离散占位网格处理等
+        # 7.env prepare
+        env.prepare()
 
-        # 8.创建主循环
+        # 8. main loop
         for traj_idx, traj in enumerate(vtrajs):
             if traj_idx % 10 != 0:
                 continue
@@ -67,7 +65,7 @@ if __name__ == '__main__':
                     max_area_conv = conv
             init_p_loc = max_area_conv.center
             env.init_agents_state(p_loc=init_p_loc, p_fwd=[0, 1], v_loc=[0, 1], v_fwd=[0, 1])
-
+            # 9. main thread
             env.reset()
             while True:
                 d = env.step()
